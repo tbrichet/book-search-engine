96b7024fc94594c5bec7ed64d3cd00523d9775de
T Brichetto <TaraPersonal@MacBook-Air.local>
2021-01-06 01:20:55 -0700

    typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,19 @@
+const { User, Book} = require('../models')
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+      // get specific user
+      me: async (parent, args, context) => {
+          if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+              .select('-v -password')
+
+              return userData;
+          }
+          throw new AuthenticationError('Not logged in');
       }
     }
   };
   
   module.exports = resolvers;
+
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,23 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
   }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+    bookCount: Int
+  }
+  
 `;
 
 // export the typeDefs
